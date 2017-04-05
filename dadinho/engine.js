1cbfd3e82540ddd3c8dcbb729d01fdfd776fd378
jribacruz <jribacruz@gmail.com>
2017-04-05 18:12:18 -0300

    estrutura inicial do jogo

---
+++ dadinho/engine.js
@@ -0,0 +1,8 @@
+/*
+ * Classe que motor do dadinho.
+ */
+module.exports = class Engine {
+    constructor() {
+        console.log('Constructor...');
+    }
+}
---
+++ dadinho/index.js
@@ -0,0 +1,3 @@
+const Engine = require('./engine.js');
+
+var room = new Engine();
