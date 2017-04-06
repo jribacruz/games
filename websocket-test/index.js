7aaa08da06217fb88ac43d2e5cf875c5d65168da
jribacruz <jribacruz@gmail.com>
2017-04-05 21:25:56 -0300

    ajustes

--- websocket-test/index.js
+++ websocket-test/index.js
@@ -2,8 +2,8 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080, path: '/dadinho' });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
+wss.on('connection', (ws) => {
+    ws.on('message', (data) => {
         // Broadcast to everyone else.
         wss.clients.forEach(function each(client) {
             if (client !== ws) {
