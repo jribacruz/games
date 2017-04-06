2f70bba560227f75a265a85f08714321251ee9f4
jribacruz <jribacruz@gmail.com>
2017-04-06 11:25:32 -0300

    inclusao de identificação de conexão

--- websocket-test/index.js
+++ websocket-test/index.js
@@ -1,9 +1,14 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080, path: '/dadinho' });
+const wss = new WebSocket.Server({ port: 8097, path: '/dadinho' });
 
 wss.on('connection', (ws) => {
+    console.log('conectado...');
+    let userId = ws.upgradeReq.headers['sec-websocket-key'];
+    console.log(`Usuário conectado: ${userId}`);
     ws.on('message', (data) => {
+        let messageUserId = ws.upgradeReq.headers['sec-websocket-key'];
+        console.log(`Mensagem de: ${messageUserId}`);
         // Broadcast to everyone else.
         wss.clients.forEach(function each(client) {
             if (client !== ws) {
