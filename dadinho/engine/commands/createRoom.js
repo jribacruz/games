4fe1f4cfde3d0e9440e20a97f527415095d9f3ce
jribacruz <jribacruz@gmail.com>
2017-04-06 21:41:12 -0300

    add da sala ao context

--- dadinho/engine/commands/createRoom.js
+++ dadinho/engine/commands/createRoom.js
@@ -9,7 +9,7 @@ module.exports = function createRoomCommand(context, request) {
     /**
      * 
      */
-    this._genRoomId = _genRoomId;
+    this._genRoomId = _genRoomId; 
 
 
     // Verifica se há uma nome de sala na requisição
@@ -19,6 +19,7 @@ module.exports = function createRoomCommand(context, request) {
             let room = new Room();
             room.name = request.name;
             room.id = context.rooms[room.id] = _genRoomId(room.name);
+            context.rooms.push(room);
 
             let commandResponse = {
                 type: 'create-room',
@@ -31,7 +32,7 @@ module.exports = function createRoomCommand(context, request) {
         }
     }
 
-    function _genRoomId(roomName) {
+     function _genRoomId(roomName) {
         let salt = (new Date()).valueOf().toString();
         var md5sum = crypto.createHash('md5');
         return md5sum.update(`${salt}${roomName}`).digest('hex');
